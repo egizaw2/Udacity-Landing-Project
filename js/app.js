7ed293a5c22fdbd67e0f5928fc2230f03ea8916c
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-04-09 11:15:08 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,6 @@ function sections() {
 sections();
 
 
-
 // add scroll to event
 function scrollToAnchor() {
     nav.addEventListener('click', function (scroll) {
@@ -31,7 +30,7 @@ function scrollToAnchor() {
     });
 };
 //Tried to change color of nav icons when clicked
-const color = document.querySelectorAll('section');
+const sectionNavbar = document.querySelectorAll('section');
 function myFunction() {
     nav.addEventListener('click', function (event) {
         document.getElementById("navbar__list").style.color = "rgb(136, 203, 171)";
@@ -46,3 +45,24 @@ function cursor() {
 cursor();
 
 element.scrollToTop({ top: 0, left: 0, behavior: 'smooth' });
+
+var navMenu = document.getElementById("navbar__menu");
+
+// Get all buttons with class="btn" inside the container
+var menuLink = navMenu.getElementsByClassName("menu__link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < menuLink.length; i++) {
+  menuLink[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    //  current/clicked button
+    this.className += " active";
+  });
+}
+
